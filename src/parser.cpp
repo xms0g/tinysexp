322af1c6d6788c08cdae120e17dea27fc76e343a
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2024-11-24 18:15:55 +0300

    feat(parser): error handling

--- src/parser.cpp
+++ src/parser.cpp
@@ -3,8 +3,9 @@
 #include "visitors.hpp"
 
 namespace {
-constexpr const char* MISSING_PAREN_ERROR = "Missing Parenthesis";
-constexpr const char* EXPECTED_INT_ERROR = "Expected INT";
+constexpr const char* MISSING_PAREN_ERROR = "missing parenthesis";
+constexpr const char* EXPECTED_INT_ERROR = "expected int";
+constexpr const char* VAR_NOT_DEFINED = " is not defined";
 }
 
 Parser::Parser(const char* fn, Lexer& lexer) : mFileName(fn), mLexer(lexer), mTokenIndex(-1) {}
@@ -91,24 +92,31 @@ ExprPtr Parser::parsePrint() {
         consume(TokenType::RPAREN, MISSING_PAREN_ERROR);
     } else {
         statement = parseAtom();
+        std::string sstmnt = StringVisitor::getResult(statement);
+        auto found = symbolTable.find(sstmnt);
+        if (found == symbolTable.end()) {
+            throw InvalidSyntaxError(mFileName, (sstmnt + VAR_NOT_DEFINED).c_str(), 0);
+        }
     }
 
     return std::make_unique<PrintExpr>(statement);
 }
 
 ExprPtr Parser::parseDotimes() {
-    ExprPtr statement, iterationCount;
+    ExprPtr statement, name, value;
     advance();
 
     consume(TokenType::LPAREN, MISSING_PAREN_ERROR);
-    advance();
-    iterationCount = parseAtom();
+    name = parseAtom();
+    value = parseAtom();
+
+    symbolTable.emplace(StringVisitor::getResult(name), IntVisitor::getResult(value));
     consume(TokenType::RPAREN, MISSING_PAREN_ERROR);
 
     if (mCurrentToken.type == TokenType::LPAREN)
         statement = parseExpr();
 
-    return std::make_unique<DotimesExpr>(iterationCount, statement);
+    return std::make_unique<DotimesExpr>(value, statement);
 }
 
 ExprPtr Parser::parseLet() {
