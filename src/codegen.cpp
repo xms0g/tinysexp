a4fc54f084ef93b8855a974dc0dd4e46bdc12719
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2024-11-28 22:17:39 +0300

    fix(codegen): minor

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -32,11 +32,12 @@ void CodeGen::emitBinOp(const BinOpExpr& binop) {
 
     if (binop.lhs->type() == ExprType::INT) {
         lhsi = binop.lhs->asNum().n;
+        putVar(binop.lhs->asNum());
     } else if (binop.lhs->type() == ExprType::VAR) {
         lhsi = binop.lhs->asVar().value->asNum().n;
+        putVar(binop.lhs->asVar().value->asNum());
     }
 
-    putVar(binop.lhs->asVar().value->asNum());
     generatedCode += ">";
 
     if (binop.rhs->type() == ExprType::INT) {
