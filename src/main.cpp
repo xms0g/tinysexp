7fc94c5beaf6d77d4183b2963c3298c3f2acc3eb
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2024-12-31 02:03:36 +0300

    feat(codegen): version bumped

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@
 #include "exceptions.hpp"
 
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 7
-#define VERSION_PATCH 12
+#define VERSION_MINOR 8
+#define VERSION_PATCH 0
 
 #define STRINGIFY0(s) # s
 #define STRINGIFY(s) STRINGIFY0(s)
