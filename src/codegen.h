6ebc8fbb5387bc9601490d031a4ba62fec6c5fa3
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2025-02-08 03:53:25 +0300

    refactor(codegen): some improvement

--- src/codegen.h
+++ src/codegen.h
@@ -49,77 +49,77 @@ public:
     const char* name(uint32_t id, int size);
 
 private:
-
     Register registers[REGISTER_COUNT] = {
-            {RAX,   SCRATCH,         false},
-            {RCX,   SCRATCH | PARAM, false},
-            {RDX,   SCRATCH | PARAM, false},
-            {RDI,   SCRATCH | PARAM, false},
-            {RSI,   SCRATCH | PARAM, false},
-            {R8,    SCRATCH | PARAM, false},
-            {R9,    SCRATCH | PARAM, false},
-            {R10,   SCRATCH | CHAIN, false},
-            {R11,   SCRATCH,         false},
-            {RBX,   PRESERVED,       false},
-            {R12,   PRESERVED,       false},
-            {R13,   PRESERVED,       false},
-            {R14,   PRESERVED,       false},
-            {R15,   PRESERVED,       false},
-            {xmm0,  SSE | PARAM,     false},
-            {xmm1,  SSE | PARAM,     false},
-            {xmm2,  SSE | PARAM,     false},
-            {xmm3,  SSE | PARAM,     false},
-            {xmm4,  SSE | PARAM,     false},
-            {xmm5,  SSE | PARAM,     false},
-            {xmm6,  SSE | PARAM,     false},
-            {xmm7,  SSE | PARAM,     false},
-            {xmm8,  SSE,             false},
-            {xmm9,  SSE,             false},
-            {xmm10, SSE,             false},
-            {xmm11, SSE,             false},
-            {xmm12, SSE,             false},
-            {xmm13, SSE,             false},
-            {xmm14, SSE,             false},
-            {xmm15, SSE,             false},
+        {RAX, SCRATCH, false},
+        {RCX, SCRATCH | PARAM, false},
+        {RDX, SCRATCH | PARAM, false},
+        {RDI, SCRATCH | PARAM, false},
+        {RSI, SCRATCH | PARAM, false},
+        {R8, SCRATCH | PARAM, false},
+        {R9, SCRATCH | PARAM, false},
+        {R10, SCRATCH | CHAIN, false},
+        {R11, SCRATCH, false},
+        {RBX, PRESERVED, false},
+        {R12, PRESERVED, false},
+        {R13, PRESERVED, false},
+        {R14, PRESERVED, false},
+        {R15, PRESERVED, false},
+        {xmm0, SSE | PARAM, false},
+        {xmm1, SSE | PARAM, false},
+        {xmm2, SSE | PARAM, false},
+        {xmm3, SSE | PARAM, false},
+        {xmm4, SSE | PARAM, false},
+        {xmm5, SSE | PARAM, false},
+        {xmm6, SSE | PARAM, false},
+        {xmm7, SSE | PARAM, false},
+        {xmm8, SSE, false},
+        {xmm9, SSE, false},
+        {xmm10, SSE, false},
+        {xmm11, SSE, false},
+        {xmm12, SSE, false},
+        {xmm13, SSE, false},
+        {xmm14, SSE, false},
+        {xmm15, SSE, false},
     };
 
     static constexpr const char* registerNames[REGISTER_COUNT][SIZE_COUNT] = {
-            {"rax",   "eax",  "ax",   "ah", "al"},
-            {"rcx",   "ecx",  "cx",   "ch", "cl"},
-            {"rdx",   "edx",  "dx",   "dh", "dl"},
-            {"rdi",   "edi",  "di",   "",   "dil"},
-            {"rsi",   "esi",  "si",   "",   "sil"},
-            {"r8",    "r8d",  "r8w",  "",   "r8b"},
-            {"r9",    "r9d",  "r9w",  "",   "r9b"},
-            {"r10",   "r10d", "r10w", "",   "r10b"},
-            {"r11",   "r11d", "r11w", "",   "r11b"},
-            {"rbx",   "ebx",  "bx",   "bh", "bl"},
-            {"r12",   "r12d", "r12w", "",   "r12b"},
-            {"r13",   "r13d", "r13w", "",   "r13b"},
-            {"r14",   "r14d", "r14w", "",   "r14b"},
-            {"r15",   "r15d", "r15w", "",   "r15b"},
-            {"xmm0",  "",     "",     "",   ""},
-            {"xmm1",  "",     "",     "",   ""},
-            {"xmm2",  "",     "",     "",   ""},
-            {"xmm3",  "",     "",     "",   ""},
-            {"xmm4",  "",     "",     "",   ""},
-            {"xmm5",  "",     "",     "",   ""},
-            {"xmm6",  "",     "",     "",   ""},
-            {"xmm7",  "",     "",     "",   ""},
-            {"xmm8",  "",     "",     "",   ""},
-            {"xmm9",  "",     "",     "",   ""},
-            {"xmm10", "",     "",     "",   ""},
-            {"xmm11", "",     "",     "",   ""},
-            {"xmm12", "",     "",     "",   ""},
-            {"xmm13", "",     "",     "",   ""},
-            {"xmm14", "",     "",     "",   ""},
-            {"xmm15", "",     "",     "",   ""},
+        {"rax", "eax", "ax", "ah", "al"},
+        {"rcx", "ecx", "cx", "ch", "cl"},
+        {"rdx", "edx", "dx", "dh", "dl"},
+        {"rdi", "edi", "di", "", "dil"},
+        {"rsi", "esi", "si", "", "sil"},
+        {"r8", "r8d", "r8w", "", "r8b"},
+        {"r9", "r9d", "r9w", "", "r9b"},
+        {"r10", "r10d", "r10w", "", "r10b"},
+        {"r11", "r11d", "r11w", "", "r11b"},
+        {"rbx", "ebx", "bx", "bh", "bl"},
+        {"r12", "r12d", "r12w", "", "r12b"},
+        {"r13", "r13d", "r13w", "", "r13b"},
+        {"r14", "r14d", "r14w", "", "r14b"},
+        {"r15", "r15d", "r15w", "", "r15b"},
+        {"xmm0", "", "", "", ""},
+        {"xmm1", "", "", "", ""},
+        {"xmm2", "", "", "", ""},
+        {"xmm3", "", "", "", ""},
+        {"xmm4", "", "", "", ""},
+        {"xmm5", "", "", "", ""},
+        {"xmm6", "", "", "", ""},
+        {"xmm7", "", "", "", ""},
+        {"xmm8", "", "", "", ""},
+        {"xmm9", "", "", "", ""},
+        {"xmm10", "", "", "", ""},
+        {"xmm11", "", "", "", ""},
+        {"xmm12", "", "", "", ""},
+        {"xmm13", "", "", "", ""},
+        {"xmm14", "", "", "", ""},
+        {"xmm15", "", "", "", ""},
     };
 };
 
 class CodeGen {
 public:
-    CodeGen() : currentStackOffset(8), currentLabelCount(0) {}
+    CodeGen() : currentStackOffset(8), currentLabelCount(0) {
+    }
 
     std::string emit(const ExprPtr& ast);
 
@@ -193,18 +193,18 @@ private:
     int currentStackOffset;
     std::unordered_map<std::string, int> stackOffsets;
     // Sections
-    std::unordered_map<std::string, std::vector<std::pair<std::string, std::string>>> sections;
+    std::unordered_map<std::string, std::vector<std::pair<std::string, std::string> > > sections;
 
     static constexpr const char* memorySize[SIZE_COUNT] = {
-            "qword", "dword", "word", "byte", "byte"
+        "qword", "dword", "word", "byte", "byte"
     };
 
     static constexpr const char* dataSizeInitialized[SIZE_COUNT] = {
-            "dq", "dd", "dw", "db", "db"
+        "dq", "dd", "dw", "db", "db"
     };
 
     static constexpr const char* dataSizeUninitialized[SIZE_COUNT] = {
-            "resq", "resd", "resw", "resb", "resb"
+        "resq", "resd", "resw", "resb", "resb"
     };
 };
 
