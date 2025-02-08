bde8698404ae9cc3715005170dfa59628ca1893a
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2025-02-08 03:26:26 +0300

    refactor(exceptions): some improvement

--- src/exceptions.hpp
+++ src/exceptions.hpp
@@ -6,25 +6,25 @@
 #include <string>
 
 /* Syntax Errors */
-constexpr const char* MISSING_PAREN_ERROR = "Missing parenthesis";
-constexpr const char* EXPECTED_NUMBER_ERROR = "Expected int or double";
-constexpr const char* EXPECTED_ELEMS_NUMBER_ERROR = "Too few elements in '{}'";
-constexpr const char* OP_INVALID_NUMBER_OF_ARGS_ERROR = "The function '{}' is called with two arguments, but wants exactly one.\n"
+constexpr auto MISSING_PAREN_ERROR = "Missing parenthesis";
+constexpr auto EXPECTED_NUMBER_ERROR = "Expected int or double";
+constexpr auto EXPECTED_ELEMS_NUMBER_ERROR = "Too few elements in '{}'";
+constexpr auto OP_INVALID_NUMBER_OF_ARGS_ERROR = "The function '{}' is called with two arguments, but wants exactly one.\n"
                                                         "Invalid number of arguments: {}";
 /* Semantic Errors */
 // Variables
-constexpr const char* UNBOUND_VAR_ERROR = "The variable '{}' is unbound";
-constexpr const char* CONSTANT_VAR_ERROR = "'{}' is a constant";
-constexpr const char* CONSTANT_VAR_DECL_ERROR = "Constant variable '{}' is not allowed here";
-constexpr const char* GLOBAL_VAR_DECL_ERROR = "Global variable '{}' is not allowed here";
-constexpr const char* MULTIPLE_DECL_ERROR = "The variable '{}' occurs more than once";
-constexpr const char* NOT_NUMBER_ERROR = "The value '{}' is not of type number";
-constexpr const char* NOT_INT_ERROR = "The value '{}' is not of type INTEGER";
+constexpr auto UNBOUND_VAR_ERROR = "The variable '{}' is unbound";
+constexpr auto CONSTANT_VAR_ERROR = "'{}' is a constant";
+constexpr auto CONSTANT_VAR_DECL_ERROR = "Constant variable '{}' is not allowed here";
+constexpr auto GLOBAL_VAR_DECL_ERROR = "Global variable '{}' is not allowed here";
+constexpr auto MULTIPLE_DECL_ERROR = "The variable '{}' occurs more than once";
+constexpr auto NOT_NUMBER_ERROR = "The value '{}' is not of type number";
+constexpr auto NOT_INT_ERROR = "The value '{}' is not of type INTEGER";
 
 // Functions
-constexpr const char* FUNC_UNDEFINED_ERROR = "The function '{}' is undefined";
-constexpr const char* FUNC_INVALID_NUMBER_OF_ARGS_ERROR = "'{}' Invalid number of arguments: {}";
-constexpr const char* FUNC_DEF_ERROR = "Function '{}' definition is not allowed here";
+constexpr auto FUNC_UNDEFINED_ERROR = "The function '{}' is undefined";
+constexpr auto FUNC_INVALID_NUMBER_OF_ARGS_ERROR = "'{}' Invalid number of arguments: {}";
+constexpr auto FUNC_DEF_ERROR = "Function '{}' definition is not allowed here";
 
 #define ERROR(STR, ...) std::format(STR, __VA_ARGS__).c_str()
 
@@ -44,19 +44,19 @@ private:
     std::string msg;
 };
 
-class IllegalCharError : public IError {
+class IllegalCharError final : public IError {
 public:
     IllegalCharError(const char* fn, const char* detail, int ln) :
             IError("Illegal Character: ", fn, detail, ln) {}
 };
 
-class InvalidSyntaxError : public IError {
+class InvalidSyntaxError final : public IError {
 public:
     explicit InvalidSyntaxError(const char* fn, const char* detail, int ln) :
             IError("Invalid Syntax: ", fn, detail, ln) {}
 };
 
-class SemanticError : public IError {
+class SemanticError final : public IError {
 public:
     explicit SemanticError(const char* fn, const char* detail, int ln) :
             IError("Error: ", fn, detail, ln) {}
