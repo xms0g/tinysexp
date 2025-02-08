d45ec13964dd2de702d5f6b44d6afda1948180f1
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2025-02-08 03:53:44 +0300

    refactor(exception): some improvement

--- src/exceptions.hpp
+++ src/exceptions.hpp
@@ -9,8 +9,9 @@
 constexpr auto MISSING_PAREN_ERROR = "Missing parenthesis";
 constexpr auto EXPECTED_NUMBER_ERROR = "Expected int or double";
 constexpr auto EXPECTED_ELEMS_NUMBER_ERROR = "Too few elements in '{}'";
-constexpr auto OP_INVALID_NUMBER_OF_ARGS_ERROR = "The function '{}' is called with two arguments, but wants exactly one.\n"
-                                                        "Invalid number of arguments: {}";
+constexpr auto OP_INVALID_NUMBER_OF_ARGS_ERROR =
+        "The function '{}' is called with two arguments, but wants exactly one.\n"
+        "Invalid number of arguments: {}";
 /* Semantic Errors */
 // Variables
 constexpr auto UNBOUND_VAR_ERROR = "The variable '{}' is unbound";
@@ -46,20 +47,21 @@ private:
 
 class IllegalCharError final : public IError {
 public:
-    IllegalCharError(const char* fn, const char* detail, int ln) :
-            IError("Illegal Character: ", fn, detail, ln) {}
+    IllegalCharError(const char* fn, const char* detail, int ln) : IError("Illegal Character: ", fn, detail, ln) {
+    }
 };
 
 class InvalidSyntaxError final : public IError {
 public:
-    explicit InvalidSyntaxError(const char* fn, const char* detail, int ln) :
-            IError("Invalid Syntax: ", fn, detail, ln) {}
+    explicit InvalidSyntaxError(const char* fn, const char* detail, int ln) : IError(
+        "Invalid Syntax: ", fn, detail, ln) {
+    }
 };
 
 class SemanticError final : public IError {
 public:
-    explicit SemanticError(const char* fn, const char* detail, int ln) :
-            IError("Error: ", fn, detail, ln) {}
+    explicit SemanticError(const char* fn, const char* detail, int ln) : IError("Error: ", fn, detail, ln) {
+    }
 };
 
 #endif
