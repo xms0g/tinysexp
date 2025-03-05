e7e2e939e1c69f570bf86147d4d5febeb98e84dc
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2025-03-05 16:07:37 +0300

    build(builder): added windows command getconf

--- build/builder.sh
+++ build/builder.sh
@@ -6,6 +6,8 @@ if [ "$OS" = "Linux" ]; then
   NPROCS="$(nproc --all)"
 elif [ "$OS" = "Darwin" ] || [ "$(echo "$OS" | grep -q BSD)" = "BSD" ]; then
   NPROCS="$(sysctl -n hw.ncpu)"
+else
+  NPROCS="$(getconf _NPROCESSORS_ONLN)"
 fi
 
 cmake -DCMAKE_BUILD_TYPE=Release ../.. && cmake --build . --parallel "$NPROCS"
