5736456e69fc3972eec29927f7803ef4f20bdde5
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2026-09-05 22:50:54 +0300

    chore(examples): remove unused `factorial` and `fibonacci` test files

--- test/factorial.lisp
+++
@@ -1,6 +0,0 @@
-(defun factorial (n)
-    (if (= n 0)
-        1
-        (* n (factorial (- n 1)))))
-
-(factorial 10)
--- test/fibonacci.lisp
+++
@@ -1,6 +0,0 @@
-(defun fibonacci (n)
-  (if (<= n 1)
-    n
-    (+ (fibonacci (- n 1)) (fibonacci (- n 2)))))
-
-(fibonacci 6)
